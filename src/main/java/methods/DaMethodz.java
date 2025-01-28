9f593a9028efa76d12198778ac72502355d57bb7
Gabriel Cabrejas <gabriel@cabrejas.net>
2025-01-28 15:52:38 +0000

    wip

---
+++ src/main/java/methods/DaMethodz.java
@@ -0,0 +1,13 @@
+package methods;
+
+// import java.util.Scanner;
+
+public class DaMethodz {
+  public static void main(String[] args) {
+    System.out.println(Sum(4, 5));
+  }
+
+  public static int Sum(int a, int b) {
+    return a + b;
+  }
+}
