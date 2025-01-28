fd8b12e23cf91bec189a307e9cc5e485f0dcda4b
Gabriel Cabrejas <gabriel@cabrejas.net>
2025-01-28 17:21:25 +0000

    wip

---
+++ src/main/java/geo/Rectangle.java
@@ -0,0 +1,46 @@
+package geo;
+
+public class Rectangle {
+  private double width;
+  private double height;
+
+  public Rectangle(double width, double height) {
+    this.height = height;
+    this.width = width;
+  }
+
+  public double getArea() {
+    return width * height;
+  }
+
+  public double getPerimeter() {
+    return 2 * (width + height);
+  }
+
+  public void setWidth(double width) {
+    this.width = width;
+  }
+
+  public void setHeight(double height) {
+    this.height = height;
+  }
+
+  public double getWidth() {
+    return width;
+  }
+
+  public double getHeight() {
+    return height;
+  }
+
+  public static void main(String[] args) {
+    Rectangle r = new Rectangle(3, 7);
+    System.out.println(r.getArea());
+    System.out.println(r.getPerimeter());
+    System.out.println("================================");
+    r.setWidth(5);
+    r.setHeight(4);
+    System.out.println(r.getArea());
+    System.out.println(r.getPerimeter());
+  }
+}
---
+++ src/main/java/methods/Greet.java
@@ -0,0 +1,20 @@
+package methods;
+
+import java.util.Scanner;
+
+public class Greet {
+  public static int gaga = 7;
+  public static void main(String[] args) {
+    String theName = doGreet();
+    System.out.println(gaga);
+  }
+
+  public static String doGreet() {
+    System.out.println("Was yer name, bud?");
+    Scanner scanner = new Scanner(System.in);
+    String name = scanner.nextLine();
+    System.out.println("So your name is " + name + "?");
+    scanner.close();
+    return name;
+  }
+}
