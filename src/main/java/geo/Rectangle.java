f1844596f03307dbf4ede401cd4968a7c01b435a
Gabriel Cabrejas <gabriel@cabrejas.net>
2025-01-28 17:35:25 +0000

    wip

--- src/main/java/geo/Rectangle.java
+++ src/main/java/geo/Rectangle.java
@@ -1,35 +1,35 @@
 package geo;
 
 public class Rectangle {
-  private double width;
-  private double height;
+    private double width;
+    private double height;
 
-  public Rectangle(double width, double height) {
-    setHeight(height);
-    setWidth(width);
-  }
+    public Rectangle(double width, double height) {
+        setHeight(height);
+        setWidth(width);
+    }
 
-  public double getArea() {
-    return width * height;
-  }
+    public double getArea() {
+        return width * height;
+    }
 
-  public double getPerimeter() {
-    return 2 * (width + height);
-  }
+    public double getPerimeter() {
+        return 2 * (width + height);
+    }
 
-  public void setWidth(double width) {
-    this.width = width;
-  }
+    public void setWidth(double width) {
+        this.width = width;
+    }
 
-  public void setHeight(double height) {
-    this.height = height;
-  }
+    public void setHeight(double height) {
+        this.height = height;
+    }
 
-  public double getWidth() {
-    return width;
-  }
+    public double getWidth() {
+        return width;
+    }
 
-  public double getHeight() {
-    return height;
-  }
+    public double getHeight() {
+        return height;
+    }
 }
