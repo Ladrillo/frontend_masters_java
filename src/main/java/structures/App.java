76444e813f6f53f7bb30fb65bb67d1663ea9990c
Gabriel Cabrejas <gabriel@cabrejas.net>
2025-01-31 17:34:50 +0000

    enhanced loop

--- src/main/java/structures/App.java
+++ src/main/java/structures/App.java
@@ -13,7 +13,7 @@ public class App {
     }
 
     public static void setDemo() {
-        Set fruits = new HashSet();
+        Set<String> fruits = new HashSet<String>();
         fruits.add("apple");
         fruits.add("banana");
         fruits.add("lemon");
@@ -31,19 +31,23 @@ public class App {
         System.out.println(fruits.contains("lemon"));
         System.out.println(fruits.size());
 
-
-        List cars = new ArrayList();
+        List<String> cars = new ArrayList<String>();
         cars.add("Prius");
         cars.add("Tacoma");
         cars.add("Celica");
         System.out.println(cars);
         System.out.println(cars.get(0));
 
-        Map fruitCalories = new HashMap();
+        Map<String, Integer> fruitCalories = new HashMap<>();
         fruitCalories.put("lemon", 45);
         fruitCalories.putIfAbsent("apple", 34);
+        fruitCalories.putIfAbsent("orange", 27);
         System.out.println(fruitCalories);
         System.out.println(fruitCalories.get("lemon"));
         System.out.println(fruitCalories.containsKey("lemon"));
+
+        for (Map.Entry<String, Integer> calInfo : fruitCalories.entrySet()) {
+            System.out.println(calInfo.getKey() + " has " + calInfo.getValue() + " calories.");
+        }
     }
 }
